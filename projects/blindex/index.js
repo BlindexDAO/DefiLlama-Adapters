c13be9218674be14dd9ad14a98313d3b4c53bfad
Or Kazaz <or.kazaz89@gmail.com>
2021-12-09 22:39:53 +0200

    Fixed BDX collateral fetching

--- projects/blindex/index.js
+++ projects/blindex/index.js
@@ -1,5 +1,5 @@
 const sdk = require("@defillama/sdk");
-const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
+// const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
 
 //-------------------------------------------------------------------------------------------------------------
 // How to add a new chain?
@@ -9,42 +9,51 @@ const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
 // 4. Add your new chain to the 'sumChainTvls' function in the export module
 //-------------------------------------------------------------------------------------------------------------
 
-const chains = [
-  {
-    name: "rsk",
-    uniswapFactoryAddress: "", // TODO: Add missing information
-    bdxTokenAddress: "", // TODO: Add missing information
+// Test RSK:
+// Go to @defilama/sdk/build/computetvl/blocks.js and add 'rsk' to the chainsForBlocks array
+
+const chains = {
+  rsk: {
+    uniswapFactoryAddress: "0x6d0aE8f3da7A451A82B48594E91Bf9d79491971d", // TODO: Add missing information
+    bdxTokenAddress: "0xB3dd46A470b2C3df15931238c61C49CDf429DD9a",
     wrappedNativeTokenAddress: "0x542fda317318ebf1d3deaf76e0b632741a7e677d",
     wrappedNativeTokenName: "wrbtc",
-    coingeckoNativeTokenId: "rbtc",
+    coingeckoNativeTokenId: "rootstock",
     bdstables: [
       {
         name: "BDEU",
-        address: "", // TODO: Add missing information
+        address: "0x09b070184E6b57475d5993D9Dd8157f0273EE230",
       },
     ],
   },
-];
+};
 
 async function getBdxBalanaceOfBDStable(
   block,
-  chain,
+  chainName,
   bdstableContract,
   bdxTokenAddress
 ) {
-  return (
+  let bdxBalance = (
     await sdk.api.erc20.balanceOf({
       target: bdxTokenAddress,
       owner: bdstableContract,
-      chain,
+      chain: chainName,
       block,
     })
   ).output;
+
+  const bdxDecimals = (await sdk.api.erc20.decimals(bdxTokenAddress, chainName))
+    .output;
+  return bdxBalance / 10 ** bdxDecimals;
 }
 
-async function rskTvl(timestamp, block) {
-  return tvl("rsk", timestamp, block);
-}
+const rsk = async function rskTvl(timestamp, ethBlock, chainblocks) {
+  console.log("Network: RSK");
+  console.log("Timestamp:", timestamp);
+  console.log("Block:", chainblocks["rsk"]);
+  return tvl("rsk", timestamp, chainblocks["rsk"]);
+};
 
 async function tvl(chainName, timestamp, block) {
   const bdxTokenAddress = chains[chainName].bdxTokenAddress;
@@ -58,13 +67,16 @@ async function tvl(chainName, timestamp, block) {
   //=======
   // TODO: What's the core token all about here? Is it actually WRBTC?
   // TODO: What's the whitelist token all about? What is it being used for in the function? (in our case it's BDX)
-  const ammTvlInWRBTC = calculateUsdUniTvl(
-    uniswapFactoryAddress,
-    chainName,
-    wrappedNativeTokenAddress,
-    [bdxTokenAddress],
-    wrappedNativeTokenName
-  )[[wrappedNativeTokenName]];
+  let ammTvlInWRBTC = 0;
+  // ammTvlInWRBTC = calculateUsdUniTvl(
+  //   uniswapFactoryAddress,
+  //   chainName,
+  //   wrappedNativeTokenAddress,
+  //   [bdxTokenAddress],
+  //   wrappedNativeTokenName
+  // )[[wrappedNativeTokenName]];
+
+  // console.log(ammTvlInWRBTC);
   // TODO: Delete this comment: calculateUsdUniTvl returns: { [coreAssetName]: (coreBalance) / (10 ** decimals) }
 
   //============
@@ -78,7 +90,7 @@ async function tvl(chainName, timestamp, block) {
   //================
   let bdxColleteralAmount = 0;
   for (let index = 0; index < bdstables.length; index++) {
-    bdxColleteralAmount += getBdxBalanaceOfBDStable(
+    bdxColleteralAmount += await getBdxBalanaceOfBDStable(
       block,
       chainName,
       bdstables[index].address,
@@ -86,12 +98,18 @@ async function tvl(chainName, timestamp, block) {
     );
   }
 
-  const bdxCollateralInWRBTC = bdxColleteralAmount * 0; // TODO: Convert from BDX amount to native token amount
+  console.log("=====================================");
+  console.log(bdxColleteralAmount);
+  console.log("=====================================");
+  const bdxCollateralInWRBTC = bdxColleteralAmount * 1; // TODO: Convert from BDX amount to native token amount
 
-  return {
+  const balances = {
     [chains[chainName].coingeckoNativeTokenId]:
       ammTvlInWRBTC + collateralInWRBTC + bdxCollateralInWRBTC,
   };
+
+  console.log(balances);
+  return balances;
 }
 
 module.exports = {
@@ -99,7 +117,7 @@ module.exports = {
   methodology:
     "(1) AMM LP pairs - All the liquidity pools from the Factory address are used to find the LP pairs. This part of the TVL is equal to the liquidity on the AMM. (2) Collateral - All the collateral being used to support the stable coins - Bitcoin, Ethereum & BDX",
   rsk: {
-    tvl: rskTvl,
+    tvl: rsk,
   },
   tvl: sdk.util.sumChainTvls([rsk]),
 };
