69c51eaaeed7afff86115f534fa4761da7530b3c
Or Kazaz <or.kazaz89@gmail.com>
2021-12-18 16:34:43 +0200

    Loading the collaterals from BDEU

--- projects/blindex/index.js
+++ projects/blindex/index.js
@@ -1,4 +1,6 @@
 const sdk = require("@defillama/sdk");
+const { default: BigNumber } = require("bignumber.js");
+const abi = require("../helper/abis/blindex.json");
 // const { calculateUsdUniTvl } = require("../helper/getUsdUniTvl");
 
 //-------------------------------------------------------------------------------------------------------------
@@ -14,60 +16,124 @@ const sdk = require("@defillama/sdk");
 
 const chains = {
   rsk: {
-    uniswapFactoryAddress: "0x6d0aE8f3da7A451A82B48594E91Bf9d79491971d", // TODO: Add missing information
-    bdxTokenAddress: "0xB3dd46A470b2C3df15931238c61C49CDf429DD9a",
-    wrappedNativeTokenAddress: "0x542fda317318ebf1d3deaf76e0b632741a7e677d",
-    wrappedNativeTokenName: "wrbtc",
-    coingeckoNativeTokenId: "rootstock",
+    // uniswapFactoryAddress: "0x6d0aE8f3da7A451A82B48594E91Bf9d79491971d", // TODO: Add missing information
+    // bdxTokenAddress: "0xB3dd46A470b2C3df15931238c61C49CDf429DD9a",
+    bdxCoingeckoId: "bdx",
+    // wrappedNativeTokenAddress: "0x542fda317318ebf1d3deaf76e0b632741a7e677d",
+    // wrappedNativeTokenName: "wrbtc",
+
     bdstables: [
       {
         name: "BDEU",
         address: "0x09b070184E6b57475d5993D9Dd8157f0273EE230",
+        collateral: {
+          btc: {
+            wrappedTokenAddress: "0x542fda317318ebf1d3deaf76e0b632741a7e677d",
+            coingeckoTokenId: "rootstock",
+          },
+          eth: {
+            wrappedTokenAddress: "0x1d931bf8656d795e50ef6d639562c5bd8ac2b78f",
+            coingeckoTokenId: "ethereum",
+          },
+        },
       },
     ],
   },
 };
 
-async function getBdxBalanaceOfBDStable(
-  block,
-  chainName,
-  bdstableContract,
-  bdxTokenAddress
-) {
-  let bdxBalance = (
-    await sdk.api.erc20.balanceOf({
-      target: bdxTokenAddress,
-      owner: bdstableContract,
+async function getBDStableCollateralBalances(block, chainName, bdstable) {
+  const btcCoinGekoTokenId = bdstable.collateral.btc.coingeckoTokenId;
+  const ethCoinGekoTokenId = bdstable.collateral.eth.coingeckoTokenId;
+  const balances = {
+    [btcCoinGekoTokenId]: 0,
+    [ethCoinGekoTokenId]: 0,
+  };
+
+  const collateralPoolsLength = (
+    await sdk.api.abi.call({
+      target: bdstable.address,
+      abi: abi["getBdStablesPoolsLength"],
       chain: chainName,
       block,
     })
   ).output;
 
-  const bdxDecimals = (await sdk.api.erc20.decimals(bdxTokenAddress, chainName))
-    .output;
-  return bdxBalance / 10 ** bdxDecimals;
+  const bdstableCollateralPools = [];
+  for (let index = 0; index < collateralPoolsLength; index++) {
+    const poolAddress = (
+      await sdk.api.abi.call({
+        target: bdstable.address,
+        abi: abi["bdstable_pools_array"],
+        params: index,
+        chain: chainName,
+        block,
+      })
+    ).output;
+
+    bdstableCollateralPools.push(poolAddress);
+  }
+
+  for (let index = 0; index < bdstableCollateralPools.length; index++) {
+    const btcCollateralAddress = bdstable.collateral.btc.wrappedTokenAddress;
+    const ethCollateralAddress = bdstable.collateral.eth.wrappedTokenAddress;
+    balances[btcCoinGekoTokenId] += await getBalanceOfWithPercision(
+      block,
+      chainName,
+      bdstableCollateralPools[index],
+      btcCollateralAddress
+    );
+    balances[ethCoinGekoTokenId] += await getBalanceOfWithPercision(
+      block,
+      chainName,
+      bdstableCollateralPools[index],
+      ethCollateralAddress
+    );
+  }
+
+  return balances;
 }
 
-const rsk = async function rskTvl(timestamp, ethBlock, chainblocks) {
-  console.log("Network: RSK");
-  console.log("Timestamp:", timestamp);
-  console.log("Block:", chainblocks["rsk"]);
-  return tvl("rsk", timestamp, chainblocks["rsk"]);
-};
+async function getBalanceOfWithPercision(block, chainName, owner, target) {
+  let balance = (
+    await sdk.api.erc20.balanceOf({
+      target,
+      owner,
+      chain: chainName,
+      block,
+    })
+  ).output;
+
+  const decimals = (await sdk.api.erc20.decimals(target, chainName)).output;
+  return balance / 10 ** decimals;
+}
+
+function sumBalances(balancesArray) {
+  return balancesArray.reduce((balances, singleBalance) => {
+    for (const [coingeckoTokenId, amount] of Object.entries(singleBalance)) {
+      if (!balances[coingeckoTokenId]) {
+        balances[coingeckoTokenId] = 0;
+      }
+
+      balances[coingeckoTokenId] += amount;
+    }
+
+    return balances;
+  }, {});
+}
 
 async function tvl(chainName, timestamp, block) {
-  const bdxTokenAddress = chains[chainName].bdxTokenAddress;
-  const wrappedNativeTokenAddress = chains[chainName].wrappedNativeTokenAddress;
-  const wrappedNativeTokenName = chains[chainName].wrappedNativeTokenName;
-  const uniswapFactoryAddress = chains[chainName].uniswapFactoryAddress;
-  const bdstables = chains[chainName].bdstables;
+  // const bdxTokenAddress = chains[chainName].bdxTokenAddress;
+  // const wrappedNativeTokenAddress = chains[chainName].wrappedNativeTokenAddress;
+  // const wrappedNativeTokenName = chains[chainName].wrappedNativeTokenName;
+  // const uniswapFactoryAddress = chains[chainName].uniswapFactoryAddress;
+  const balancesArray = [];
 
   //=======
   // AMM
   //=======
   // TODO: What's the core token all about here? Is it actually WRBTC?
   // TODO: What's the whitelist token all about? What is it being used for in the function? (in our case it's BDX)
-  let ammTvlInWRBTC = 0;
+  // let ammTvlInWRBTC = 0;
   // ammTvlInWRBTC = calculateUsdUniTvl(
   //   uniswapFactoryAddress,
   //   chainName,
@@ -79,38 +145,42 @@ async function tvl(chainName, timestamp, block) {
   // console.log(ammTvlInWRBTC);
   // TODO: Delete this comment: calculateUsdUniTvl returns: { [coreAssetName]: (coreBalance) / (10 ** decimals) }
 
-  //============
-  // Collateral
-  //============
-  // TODO: Need to query all the pools of each BDStable using its property: bdstable_pools_array. Then need to calc all the collateral
-  const collateralInWRBTC = 0;
+  //===================
+  // Non-BDX Collateral
+  //===================
+  const bdstables = chains[chainName].bdstables;
+  for (let index = 0; index < bdstables.length; index++) {
+    balancesArray.push(
+      await getBDStableCollateralBalances(block, chainName, bdstables[index])
+    );
+  }
 
   //================
   // BDX Collateral
   //================
-  let bdxColleteralAmount = 0;
-  for (let index = 0; index < bdstables.length; index++) {
-    bdxColleteralAmount += await getBdxBalanaceOfBDStable(
-      block,
-      chainName,
-      bdstables[index].address,
-      bdxTokenAddress
-    );
-  }
+  // TODO: Uncomment whenever coingecko adds BDX price
+  const bdxBalance = {};
+  // bdxBalance[bdxCoingeckoId] = 0;
+  // for (let index = 0; index < bdstables.length; index++) {
+  //   bdxBalance[bdxCoingeckoId] += await getBalanceOfWithPercision(
+  //     block,
+  //     chainName,
+  //     bdstables[index].address,
+  //     bdxTokenAddress
+  //   );
+  // }
 
-  console.log("=====================================");
-  console.log(bdxColleteralAmount);
-  console.log("=====================================");
-  const bdxCollateralInWRBTC = bdxColleteralAmount * 1; // TODO: Convert from BDX amount to native token amount
+  // console.log("=====================================");
+  // console.log(bdxBalance);
+  // console.log("=====================================");
+  balancesArray.push(bdxBalance);
 
-  const balances = {
-    [chains[chainName].coingeckoNativeTokenId]:
-      ammTvlInWRBTC + collateralInWRBTC + bdxCollateralInWRBTC,
-  };
-
-  console.log(balances);
-  return balances;
+  return sumBalances(balancesArray);
 }
+
+const rsk = async function rskTvl(timestamp, ethBlock, chainblocks) {
+  return tvl("rsk", timestamp, chainblocks["rsk"]);
+};
 
 module.exports = {
   misrepresentedTokens: true,
@@ -119,18 +189,6 @@ module.exports = {
   rsk: {
     tvl: rsk,
   },
-  tvl: sdk.util.sumChainTvls([rsk]),
+  // TODO: Should we include this?? It calls RSK twice
+  // tvl: sdk.util.sumChainTvls([rsk]),
 };
-
-// TODO: Delete this VVS finance code
-// const factoryAddress = '0x3b44b2a187a7b3824131f8db5a74194d0a42fc15';
-// const wcroAddress = '0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23';
-// const vvsTokenAddress = '0x2D03bECE6747ADC00E1a131BBA1469C15fD11e03';
-
-// module.exports = {
-//     misrepresentedTokens: true,
-//     methodology: `(1) AMM LP pairs - All the pools from the  Factory address (${factoryAddress}) are used to find the LP pairs. TVL is equal to the liquidity on the AMM. (2) All the collateral being used to support the stale coins. (3) All the BDX collateral being used to support the stable coins.`,
-//     rsk: {
-//         tvl: calculateUsdUniTvl(factoryAddress, "cronos", wcroAddress, [vvsTokenAddress], "crypto-com-chain")
-//     }
-// }
